95bb63e0d3c2153c35b3ffa352085473fcd10a13
Nicolas VERHELST <nicolas.verhelst@rapidresponserevival.com.au>
2020-11-01 15:29:38 +1100

    Add missing doc

--- RpiRequestor/Constant.cs
+++ RpiRequestor/Constant.cs
@@ -1,8 +1,14 @@
 ﻿
 namespace TheLighthouse
 {
+    /// <summary>
+    /// Public class regrouping all the constants used in the software
+    /// </summary>
     public static class Constant
     {
+        /// <summary>
+        /// Time to wait for a new result before considering that the connection is lost (millisecond)
+        /// </summary>
         public static int TimeoutFreshnessMs => 1000;
     }
 }
